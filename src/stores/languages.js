f0c7faa98f7c1a3e4ca2fc34c65de25400ba0d51
BOUT Valentin <bout.valentin@gmail.com>
2021-03-31 22:29:30 +0200

    Fix: resolve error in languages browser

--- src/stores/languages.js
+++ src/stores/languages.js
@@ -1,5 +1,6 @@
 import { derived, writable } from 'svelte/store';
 import * as Data from '../../public/static/languages.json'
+import { LANGUE_SUPPORT } from '../constants';
 
 export let avaiblesLanguages = Object.keys(Data).slice(0, Object.keys(Data).length -1);
 
@@ -7,6 +8,6 @@ const langNav = navigator.language;
 const indO = langNav.indexOf('-');
 const finalLang = langNav.substring(0, indO >= 0 ? indO : langNav.length).toLocaleLowerCase();
 
-export let currentLanguages = writable(finalLang || "en");
+export let currentLanguages = writable(LANGUE_SUPPORT.includes(finalLang) ? finalLang : "en");
 
 export let DataLanguages = derived(currentLanguages, ($languages) => Data[$languages]);
